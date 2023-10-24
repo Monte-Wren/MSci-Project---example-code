23d037377f2b962ed15169d9d8480c9d21accfaa
Monte-Wren <87189710+Monte-Wren@users.noreply.github.com>
2023-10-24 12:13:55 +0100

    Update DLA_v2_jitted.py

--- DLA_v2_jitted.py
+++ DLA_v2_jitted.py
@@ -9,14 +9,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numba import njit
 
-#%% JITTED
+#%% JITTED: Functions jitted and called within DLA class in order to speed up code.
 
 @njit
 def distance(x, y):
+    '''
+    Calculates distance from the origin (0,0) to point (x, y)
+    Input: 
+    x: int
+    y: int
+
+    Return: float
+    '''
     return np.sqrt(x*x+y*y)
 
 @njit
 def jitCircleBound(radius):
+    '''
+    Initialise circle at a specified radius via a list of integer points
+    via equation x**2 + y**2 = radius**2
+    Input:
+    radius: float
+
+    Return: 
+    Circle: list(int)
+    '''
     circle = []
     for x in range(-int(radius), int(radius)+1):
         y_max = int(np.sqrt(radius**2-x**2))
@@ -26,10 +43,29 @@ def jitCircleBound(radius):
 
 @njit
 def jitRandPoint(circle):
+    '''
+    Return randomly chosen point from the circle perimeter.
+
+    Input:
+    Circle: list(int)
+
+    Return: tuple(int)
+    '''
     return circle[np.random.randint(0, len(circle))]
 
 @njit
 def jitGetNeighbours(radius, point):
+    '''
+    Return list of neighbouring points to specified points as long as points are
+    not outside the initialised circle of given radius.
+    
+    Input:
+    point: tuple(int)
+    radius: int
+
+    Return:
+    list(tuple(int))
+    '''
     x,y = point
     NN = [(x-1, y+1), (x, y+1), (x+1, y+1),
           (x-1, y),             (x+1, y),
@@ -39,15 +75,55 @@ def jitGetNeighbours(radius, point):
 
 
 def jitCheckStick(points, adj_points):
+    '''
+    Check if any points in points, i.e the cluster, share any points in adj_points. 
+    This determines whether the randomly walking particle sticks to the cluster.
+
+    Input:
+    points: list(tuple(int))
+    adj_points: list(tuple(int))
+
+    Return: Bool
+    '''
     return any(n in adj_points for n in points)
 
 @njit
 def jitMovePoint(adj_points):
+    '''
+    Move randomly walking particle by picking a random point in its neighbouring points
+
+    Input:
+    adj_points: list(tuple(int))
+
+    Return:
+    new_pos: tuple(int)
+    '''
     new_pos = adj_points[np.random.randint(len(adj_points))]
     return new_pos
 
  
-def jitGrow(points, boundary, radius, dr, no_points = 1):
+def jitGrow(points, boundary, radius, dr):
+    '''
+    Grow cluster by picking a random point on the perimeter of the initial circle.
+    Then move point to one of its neighbours (i.e an integer step) until one of its
+    neighbouring points includes one of the points in the cluster.
+    If the randomly walking point exits the initial circle + a small step dr, 
+    it is respawned at another random point on the circle.
+    Once the particle is registered as being stuck to the cluster, the list of points
+    of the cluster is updated, the radius of the spawn circle expanded and the list
+    of points making the spawn circle recalculated.
+
+    Input:
+    points: list(tuple(int))
+    boundary: list(tuple(int))
+    radius: float
+    dr: int
+
+    Return:
+    points: list(tuple(int))
+    radius: float
+    boundary: list(tuple(int))
+    '''
     ticker = 0
     while ticker < no_points:
         spawn = jitRandPoint(boundary)
